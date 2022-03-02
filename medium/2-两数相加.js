d247424e8df38e29e4a4ff4bcc181165adc4df01
Hacker-C <1507559148@qq.com>
2022-03-02 20:53:00 +0800

    删除无用文件

--- medium/2-两数相加.js
+++ medium/2-两数相加.js
@@ -1,8 +1,3 @@
-function ListNode(val, next) {
-  this.val = (val === undefined ? 0 : val)
-  this.next = (next === undefined ? null : next)
-}
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -16,28 +11,44 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let a = '', b = ''
-  while (l1 != null) {
-    a = l1.val + a
+  let sum = 0
+  let ret = new ListNode()
+  let cur = ret
+  // 同时遍历 l1和l2，对应节点值相加，有进位则进位
+  while (l1 && l2) {
+    sum = l1.val + l2.val + sum
+    let base = sum % 10
+    let node = new ListNode(base, null)
+    // 采用尾添加节点方法
+    cur.next = node
+    cur = node
+    if (sum >= 10) {
+      sum = 1
+    } else {
+      sum = 0
+    }
     l1 = l1.next
-  }
-  while (l2 != null) {
-    b = l2.val + b
     l2 = l2.next
   }
-  let res = (BigInt(a) + BigInt(b)).toString()
-  let ret = new ListNode()
-  for (let e of res) {
-    if (e === 'n') break
-    let node = new ListNode(parseInt(e))
-    node.next = ret.next
-    ret.next = node
+  // 长的链表继续遍历
+  let temp = l1 || l2
+  while (temp) {
+    sum = temp.val + sum
+    let base = sum % 10
+    let node = new ListNode(base, null)
+    // 采用尾添加节点方法
+    cur.next = node
+    cur = node
+    if (sum >= 10) {
+      sum = 1
+    } else {
+      sum = 0
+    }
+    temp = temp.next
+  }
+  if (sum > 0) {
+    let node = new ListNode(1, null)
+    cur.next = node
   }
   return ret.next
 };
-
-/*
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-[5,6,4]
-
-*/
