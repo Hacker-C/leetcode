724fac599cdc5042db8d3da3c463c17409e5de4a
Hacker-C <1507559148@qq.com>
2022-03-02 14:58:49 +0800

    删除多余文件

--- easy/1-两数之和.js
+++ easy/1-两数之和.js
@@ -1,10 +1,21 @@
-// 常规解法：O(n^2)
+// 哈希解法：O(n)
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let sub = target - nums[i]
+    if (map.has(sub)) {
+      return [i, map.get(sub)]
+    }
+    map.set(nums[i], i)
+  }
+};
+
+/* var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -12,6 +23,4 @@ var twoSum = function (nums, target) {
       }
     }
   }
-};
-
-console.log(twoSum([1, 2, 3], 5))
+}; */
