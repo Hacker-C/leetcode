a477c7e0e33d13269aca32c41e4c093740c80b13
Hacker-C <1507559148@qq.com>
2022-03-04 14:12:03 +0800

    136 添加第二个方法

--- easy/136.只出现一次的数字.js
+++ easy/136.只出现一次的数字.js
@@ -4,4 +4,17 @@
  */
 var singleNumber = function (nums) {
   return nums.reduce((pre, cur) => pre ^ cur)
+
+  /* const map = new Map()
+  for (let e of nums) {
+    if (map.get(e) === 1) {
+      map.set(e, 0)
+    } else {
+      map.set(e, 1)
+    }
+  }
+  for (let [k, v] of map) {
+    if (v === 1) return k
+  } */
+
 };
