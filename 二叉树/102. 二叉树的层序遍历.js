428b7efd951e05ceb97f9a70eaca9ce2df884395
Hacker-C <1507559148@qq.com>
2022-03-15 11:10:58 +0800

    二叉树层序遍历

---
+++ 二叉树/102. 二叉树的层序遍历.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (root) {
+    return [];
+  }
+  let q = [];
+  let ans = [];
+  q.push(root);
+  while (q.length) {
+    let len = q.length;
+    let temp = [];
+    for (let i = 0; i < len; i++) {
+      let node = q.shift();
+      temp.push(node.val);
+      node.left && q.push(node.left);
+      node.right && q.push(node.right);
+    }
+    ans.push(temp);
+  }
+  return ans;
+}
