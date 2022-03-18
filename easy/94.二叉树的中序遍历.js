6ad6b402bfa8cf721145d394a33620773bcb305f
Hacker-C <1507559148@qq.com>
2022-03-18 20:42:18 +0800

    迭代法实现中序遍历

--- easy/94.二叉树的中序遍历.js
+++ easy/94.二叉树的中序遍历.js
@@ -10,16 +10,51 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+
+// 方法3：迭代（栈实现）
 var inorderTraversal = function (root) {
-  let res = []
-  const sub = (node, ans = []) => {
-    if (node !== null) {
-      sub(node.left, ans)
-      ans.push(node.val)
-      sub(node.right, ans)
+  let stack = [];
+  let res = [];
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
     }
-    return ans
+    root = stack.pop();
+    res.push(root.val);
+    root = root.right;
   }
-  res = sub(root)
-  return res
+  return res;
+};
+
+// 方法2：迭代（颜色标记）
+var inorderTraversal = function (root) {
+  const [WHITE, GRAY] = [0, 1];
+  let res = [];
+  let stack = [[WHITE, root]];
+  while (stack.length) {
+    [color, node] = stack.pop();
+    if (!node) continue;
+    if (color === WHITE) {
+      stack.push([WHITE, node.right]);
+      stack.push([GRAY, node]);
+      stack.push([WHITE, node.left]);
+    } else {
+      res.push(node.val);
+    }
+  }
+  return res;
+};
+
+// 方法1：递归
+var inorderTraversal = function (root) {
+  const help = (node, res = []) => {
+    if (node) {
+      help(node.left, res);
+      res.push(node.val);
+      help(node.right, res);
+    }
+    return res;
+  };
+  return help(root);
 };
