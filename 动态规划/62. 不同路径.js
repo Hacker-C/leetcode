b938ffe531b15a58edb050284feee94a0c3bf533
Hacker-C <1507559148@qq.com>
2022-03-19 18:32:23 +0800

    62. 不同路径

---
+++ 动态规划/62. 不同路径.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  let dp = Array(m)
+    .fill('')
+    .map((_) => Array(n))
+  dp[0][0] = 1
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = 1
+  }
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = 1
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+  return dp[m - 1][n - 1]
+}
