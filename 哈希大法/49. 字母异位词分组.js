75f37233d9894567c8ce0b060d487b327e270379
Hacker-C <1507559148@qq.com>
2022-03-26 21:58:11 +0800

    lc 49

--- 哈希大法/49. 字母异位词分组.js
+++ 哈希大法/49. 字母异位词分组.js
@@ -23,5 +23,14 @@ var groupAnagrams = function (strs) {
 }
 
 // # 方法2：每个字母作为质数，质数相乘作为键，这样同类异构词肯定不会重复
+var groupAnagrams = function (strs) {
+  const t = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101
+  ]
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const getN = new Map()
+  // ...
+}
 
 groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
