98e16f96573663d108ca0902d238cdabd8eb2b99
Hacker-C <1507559148@qq.com>
2022-03-31 20:29:12 +0800

    lc 344

---
+++ 字符串/344. 反转字符串.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let l = 0,
+    r = s.length - 1
+  while (l <= r) {
+    ;[s[l], s[r]] = [s[r], s[l]]
+  }
+  return s
+}
