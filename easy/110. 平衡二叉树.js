044b16b7e2b396ccd358454eef2b36193db67eca
Hacker-C <1507559148@qq.com>
2022-04-09 14:28:23 +0800

    lc 110

---
+++ easy/110. 平衡二叉树.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  const depth = (root) => {
+    if (!root) return 0
+    return Math.max(depth(root.left), depth(root.right)) + 1
+  }
+  if (!root) return true
+  return (
+    Math.abs(depth(root.left) - depth(root.right)) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  )
+}
