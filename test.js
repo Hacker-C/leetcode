4d5c82a4732ad3ba67d71325741abffb94e03ea1
Hacker-C <1507559148@qq.com>
2022-04-11 10:20:22 +0800

    测试

--- test.js
+++ test.js
@@ -6,12 +6,13 @@ const handler = {
       trapTarget.sum = 0
       return () => temp
     }
+
     trapTarget.sum += Number(property)
     return receiver
   }
 }
 const add = new Proxy(target, handler)
 
-console.log(add[1][2][3] + 4) // 10
-console.log(add[10][20] + 30) // 60
-console.log(add[1000][2000][3000] + 4000) // 10000
+console.log(add[1][2][3] + 4)
+console.log(add[10][20] + 30)
+console.log(add[1000][2000][3000] + 4000)
