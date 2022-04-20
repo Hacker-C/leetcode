164f9ad46b7fc61598db26afc756106f6dda8542
Hacker-C <1507559148@qq.com>
2022-04-20 11:19:49 +0800

    lc 704

---
+++ 代码随想录/array/704. 二分查找.ts
@@ -0,0 +1,12 @@
+function search(nums: number[], target: number): number {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    let mid = (left + right) >> 1
+    if (nums[mid] === target) return mid
+    else if (nums[mid] > target) right = mid - 1
+    else left = mid + 1
+  }
+  return -1
+}
+console.log(search([5], 5))
