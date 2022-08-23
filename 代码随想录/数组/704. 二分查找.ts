6365af2208bd3561a1df4666489fa7c1d9e15f20
Hacker-C <1507559148@qq.com>
2022-08-23 12:01:52 +0800

    ✨ feat: 二分查找

--- 代码随想录/数组/704. 二分查找.ts
+++ 代码随想录/数组/704. 二分查找.ts
@@ -1,12 +1,13 @@
 function search(nums: number[], target: number): number {
-  let left = 0
-  let right = nums.length - 1
-  while (left <= right) {
-    let mid = (left + right) >> 1
+  let [low, high] = [0, nums.length - 1]
+  while (low <= high) {
+    let mid = (low + high) >> 1
     if (nums[mid] === target) return mid
-    else if (nums[mid] > target) right = mid - 1
-    else left = mid + 1
+    if (nums[mid] > target) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
   }
   return -1
 }
-console.log(search([5], 5))
