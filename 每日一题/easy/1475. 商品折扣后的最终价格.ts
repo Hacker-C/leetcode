05b23a4d2be991b605ebf725a5570ad4f1c542cb
Hacker-C <1507559148@qq.com>
2022-09-01 09:29:31 +0800

    ✨ feat: 每日一题

---
+++ 每日一题/easy/1475. 商品折扣后的最终价格.ts
@@ -0,0 +1,11 @@
+function finalPrices(prices: number[]): number[] {
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] < prices[i]) {
+        prices[i] -= prices[j]
+        break
+      }
+    }
+  }
+  return prices
+}
