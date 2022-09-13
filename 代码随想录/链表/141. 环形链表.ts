ce724581aae403c2c16bf2f7711572871821cbc9
Hacker-C <1507559148@qq.com>
2022-09-13 10:25:30 +0800

    ✨ feat: 环形链表

---
+++ 代码随想录/链表/141. 环形链表.ts
@@ -0,0 +1,18 @@
+function hasCycle(head: ListNode | null): boolean {
+  let slow = head, fast = head
+  while(slow && fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) return true
+  }
+  return false
+}
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
