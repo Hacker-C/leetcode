5e33cf503b6b4e1d2746b2ab5c5853e3568ac840
Hacker-C <1507559148@qq.com>
2022-09-14 12:51:28 +0800

    🔧 chore: 移除重复代码

--- 代码随想录/链表/141. 环形链表.ts
+++ 代码随想录/链表/141. 环形链表.ts
@@ -7,12 +7,3 @@ function hasCycle(head: ListNode | null): boolean {
   }
   return false
 }
-
-class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-  }
-}
